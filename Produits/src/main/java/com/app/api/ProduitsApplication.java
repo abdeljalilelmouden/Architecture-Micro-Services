7420810c657aa142d1ef6be8a62077018352af97
elmoudenen123@gmail.com <45593502+abdeljalilelmouden@users.noreply.github.com>
2020-06-04 23:42:03 +0200

    Création API pour la gestion des produits

--- Produits/src/main/java/com/app/api/ProduitsApplication.java
+++ Produits/src/main/java/com/app/api/ProduitsApplication.java
@@ -2,14 +2,12 @@ package com.app.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.config.server.EnableConfigServer;
 
-@EnableConfigServer
 @SpringBootApplication
-public class ServiceConfigApplication {
+public class ProduitsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ServiceConfigApplication.class, args);
+		SpringApplication.run(ProduitsApplication.class, args);
 	}
 
 }
