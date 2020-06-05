cfbf9adf8f531a3d50e5cc6e5504ccb0d025cf35
elmoudenen123@gmail.com <45593502+abdeljalilelmouden@users.noreply.github.com>
2020-06-05 15:22:48 +0200

    configuration service de proxy

--- Service-Proxy/src/main/java/com/app/api/ServiceProxyApplication.java
+++ Service-Proxy/src/main/java/com/app/api/ServiceProxyApplication.java
@@ -2,12 +2,21 @@ package com.app.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ServiceProxyApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServiceProxyApplication.class, args);
+	}
+	
+	@Bean
+	RouteLocator routes(RouteLocatorBuilder builder) {
+		return builder.routes()
+				.route(r->r.path("/produits/**").uri("lb://SERVICE-PRODUIT").id("r1")).build();
 	}
 
 }
